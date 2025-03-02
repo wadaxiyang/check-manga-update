e2ae99552cc393ae54aaf6eb669794f332c1bcb9
wadaxiyang <94066443+wadaxiyang@users.noreply.github.com>
2025-03-02 10:30:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from utils.run import run
 from utils.send_email import EmailServer
 
 vars = 'var.json'
-salt = "123456" # 盐
+salt = "1234568899" # 盐
 username, password, from_email, to_email,token,email_token= init(vars,salt)
 server = EmailServer("smtp.qq.com", 465, email_token) # 邮件服务
 
